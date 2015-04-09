c15215b6695645623623d4d7ac76c2b873077afb
Eric Liprandi <eric.liprandi@gmail.com>
2015-04-08 20:35:53 -0600

    Multicast with AssemblyInfo
    namespace

--- NoCoUG.PostSharp/src/NoCoUG.PostSharp.WPF/MainWindow.xaml.cs
+++ NoCoUG.PostSharp/src/NoCoUG.PostSharp.WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using NoCoUG.PostSharp.WPF.ViewModels;
 using PostSharp.Patterns.Model;
 
 namespace NoCoUG.PostSharp.WPF
@@ -14,25 +15,58 @@ namespace NoCoUG.PostSharp.WPF
         {
             InitializeComponent();
 
-            _viewModel = new MainViewModel()
+            _viewModel = new MainViewModel
             {
                 Name = "Bob",
                 Age = 37
             };
             DataContext = _viewModel;
         }
-            private void Update(object sender, RoutedEventArgs e)
+
+        private void Update(object sender, RoutedEventArgs e)
         {
             _viewModel.Name = "Mike";
             _viewModel.Age = 5;
         }
+    }
 }
 
-    [NotifyPropertyChanged]
+namespace NoCoUG.PostSharp.WPF.ViewModels
+{
+
     public class MainViewModel
     {
         public string Name { get; set; }
         public int Age { get; set; }
     }
 
+    public class MainViewModel2
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class MainViewModel3
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class MainViewModel4
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class MainViewModel5
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class MainViewModel6
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
 }
