290f20b7fc6032e590ef284959d320b6fc03b215
Eric Liprandi <eric.liprandi@gmail.com>
2015-04-08 19:26:18 -0600

    created basic solution
    grid, labels, view model, font size

---
+++ NoCoUG.PostSharp/src/NoCoUG.PostSharp.WPF/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace NoCoUG.PostSharp.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MainViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _viewModel = new MainViewModel()
+            {
+                Name = "Bob",
+                Age = 37
+            };
+            DataContext = _viewModel;
+        }
+    }
+
+    public class MainViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+}
