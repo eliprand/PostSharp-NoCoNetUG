ca63af71631c53c4ee7fe3acc19ccaad784cf4ce
Eric Liprandi <eric.liprandi@gmail.com>
2015-04-08 20:00:16 -0600

    Revert "Added Update button and code"
    This reverts commit 08b9fab027fa78f4c266f72e595edb78e2ad8a30.

--- NoCoUG.PostSharp/src/NoCoUG.PostSharp.WPF/MainWindow.xaml.cs
+++ NoCoUG.PostSharp/src/NoCoUG.PostSharp.WPF/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace NoCoUG.PostSharp.WPF
 {
@@ -24,44 +20,12 @@ namespace NoCoUG.PostSharp.WPF
             };
             DataContext = _viewModel;
         }
-
-        private void Update(object sender, RoutedEventArgs e)
-        {
-            _viewModel.Name = "Mike";
-            _viewModel.Age = 5;
-        }
     }
 
-    public class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel
     {
-        private string _name;
-        private int _age;
-
-        public string Name
-        {
-            get { return _name; }
-            set { SetValue(ref _name, value); }
-        }
-
-        public int Age
-        {
-            get { return _age; }
-            set { SetValue(ref _age, value); }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void SetValue<T>(ref T field, T value, [CallerMemberName] string name = null)
-        {
-            field = value;
-            OnPropertyChanged(name);
-        }
-
-        protected virtual void OnPropertyChanged(string name)
-        {
-            var handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(name));
-        }
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 
 }
